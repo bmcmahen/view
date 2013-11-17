94db8477fc268c52387f2dfde3ccdb429c2a1336
bmcmahen <ben.mcmahen@gmail.com>
2013-11-17 14:51:59 -0700

    first commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+
+# view
+
+  a simple view for use with component/reactive
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install bmcmahen/view
+
+## API
+
+
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,52 @@
+var dom = require('dom');
+var Emitter = require('emitter');
+var events = require('events');
+var EmitterManager = require('emitter-manager');
+var inherit = require('inherit');
+var reactive = require('reactive');
+
+/**
+ * Opinionated View. Somewhat like Backbone, but using reactive
+ * templates instead.
+ * @param {Element} el 
+ */
+
+function View(el){
+  if (!(this instanceof View)) return inherit(el, View);
+  this.el = dom(el);
+  this._bound = {};
+  this.events = events(this.el.get(), this);
+}
+
+Emitter(View.prototype);
+EmitterManager(View.prototype);
+
+View.prototype.remove = function(){
+  this.el.remove();
+};
+
+View.prototype.react = function(model){
+  this.reactive = reactive(this.el.get(), model || this, this);
+};
+
+View.prototype.find = function(sel){
+  return this.el.find(sel);
+};
+
+View.prototype.bind = function(str, method){
+  this.events.bind(str, method);
+  return this;
+};
+
+View.prototype.unbind = function(str, method){
+  this.events.unbind(str, method);
+  return this;
+};
+
+View.prototype.bound = function(method){
+  if (!this._bound[method]){
+    this._bound[method] = this[method].bind(this);
+  }
+  return this._bound[method];
+};
+
